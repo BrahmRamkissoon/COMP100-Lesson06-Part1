e1124c6f2951e36f6036c57e445b7987610aeb7f
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-13 21:55:16 -0400

    Initial Commit

---
+++ Lesson06-Part1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson06_Part1
+{
+    enum Browser 
+    {
+        IE = 1, CHROME, FIREFOX, SAFARI
+    }
+
+    class Program
+    {
+        static void Main ( string [] args )
+        {
+            int favouriteBrowser;
+            string prompt;
+            bool answer;
+
+            Console.Write( "Q1. What's your favourite browser: " );
+            Console.WriteLine( "++++++++++++++++++++++++++++++++" );
+            Console.WriteLine( "1 - IE" );
+            Console.WriteLine( "2 - Chrome" );
+            Console.WriteLine( "3 - Firefox" );
+            Console.WriteLine( "4 - Safari" );
+            Console.WriteLine( "Please make your selection (1 to 4):" );
+            prompt = Console.ReadLine();
+            Console.Write( "\n");
+            favouriteBrowser = Convert.ToInt32( prompt );
+            Console.WriteLine("++++++++++++++++++++++++++++++++");
+
+            switch ((Browser)favouriteBrowser)          // cast to ENUM
+            {
+                case Browser.IE:
+                    Console.WriteLine( "You Chose IE" );
+                    break;
+                case Browser.CHROME:
+                    Console.WriteLine( "You Chose the most popular browser" );
+                    break;
+                case Browser.FIREFOX:
+                    Console.WriteLine("That browser is pretty good");
+                    break;
+                case Browser.SAFARI:
+                    Console.WriteLine("You like apple, eh?");
+                    break;
+                default:
+                    Console.WriteLine( $"ERROR - Invalid Entry!!" );
+                    break;
+            }
+
+
+            answer = favouriteBrowser == 2 ? true : false;
+
+            // Wait for a key to bre pressed before continuing
+            Console.WriteLine("++++++++++++++++++++++++++++++++");
+            Console.WriteLine( "Press any key to exit..." );
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6___Part_1
+{
+    class Program
+    {
+        enum Browser { IE = 1, CHROME, FIREFOX, SAFARI };
+
+        static void Main(string[] args)
+        {
+            int favouriteBrowser;
+            string prompt;
+            bool answer;
+
+            Console.WriteLine("What's your Favourite Browser: ");
+            Console.WriteLine("++++++++++++++++++++++++++++++");
+            Console.WriteLine("1  -  IE");
+            Console.WriteLine("2  -  Chrome");
+            Console.WriteLine("3  _  Firefox");
+            Console.WriteLine("4  -  Safari");
+            Console.Write("Please enter make your selection (1 to 4): ");
+
+            prompt = Console.ReadLine();
+            Console.Write("\n");
+            favouriteBrowser = Convert.ToInt32(prompt);
+
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++");
+            switch ((Browser)favouriteBrowser)
+            {
+                case Browser.IE:
+                    Console.WriteLine("You chose IE");
+                    break;
+                case Browser.CHROME:
+                    Console.WriteLine("You chose the most popular browser");
+                    break;
+                case Browser.FIREFOX:
+                    Console.WriteLine("You chose Firefox");
+                    break;
+                case Browser.SAFARI:
+                    Console.WriteLine("You like apple");
+                    break;
+                default:
+                    Console.WriteLine("ERROR - Invalid Entry!!");
+                    break;
+            }
+
+           answer = (Browser)favouriteBrowser == Browser.CHROME ? true : false;
+
+            //Wait for a key to be pressed before continuing
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
